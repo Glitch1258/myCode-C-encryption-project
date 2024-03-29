3a7f63ad9dc46e7627965dc8b663a73331822fba
Glitch12358 <glitch12358@gmail.com>
2024-03-29 12:41:17 +0600

    main push key generation improved

--- generatePublicKey.c
+++ generatePublicKey.c
@@ -4,13 +4,47 @@
 long long int* showAvailablePublicKeys(long long int PHI, long long int* sizeOfarrayArgument) {
   long long int sizeOfValidPublicKeyArray = 0;
   long long int publicKeyIndex = 0;
-
+//<changed>
+  long long int maximumNumberOfPublicKeys,minimumValueOfPrivateKey;
+  int USER_INPUT_FOR_KEYS;
+//</changed>
+printf("\nCalculating number of possible public keys...\n");
   for (long long int i = 2; i < PHI; i++) {
     if (greatestCommonDivisor(i, PHI) == 1) {
       sizeOfValidPublicKeyArray++;
     }
   }
+//<changed>
+  generatePublicKeys:
+  printf("%lld possible public keys can be generated\n",sizeOfValidPublicKeyArray);
+  printf("Press 1 to generate all possible public keys : \n");
+  printf("Press 2 to limit the number of possible public keys generated : \n");
+  scanf("%d",&USER_INPUT_FOR_KEYS);
+  if(USER_INPUT_FOR_KEYS==1){
+    minimumValueOfPrivateKey=2;
 
+  }else if(USER_INPUT_FOR_KEYS==2){
+    printf("Enter maximum number of public keys to be generated : ");
+    scanf("%lld",&maximumNumberOfPublicKeys);
+
+    if(maximumNumberOfPublicKeys>PHI){
+      maximumNumberOfPublicKeys=PHI;
+    }
+    sizeOfValidPublicKeyArray = maximumNumberOfPublicKeys;
+
+    printf("Enter minimum value  of public key to be generated : ");
+    scanf("%lld",&minimumValueOfPrivateKey);
+    if(minimumValueOfPrivateKey<2){
+      minimumValueOfPrivateKey=2;
+    }
+
+  }else{
+    printf("invalid input");
+    goto generatePublicKeys;
+
+  }
+
+//</changed>
   long long int* validPublicKeyArray = (long long int*)malloc(sizeOfValidPublicKeyArray * sizeof(long long int));
 
   if (validPublicKeyArray == NULL) {
@@ -21,12 +55,16 @@ long long int* showAvailablePublicKeys(long long int PHI, long long int* sizeOfa
     errorValueAdderss = &errorValue;
     return errorValueAdderss;
   }
-
-  for (long long int i = 2; i < PHI; i++) {
+//<changed>
+  for (long long int i = minimumValueOfPrivateKey; i < PHI; i++) {
+//</changed>
     if (greatestCommonDivisor(i, PHI) == 1) {
       printf("valid public key INDEX :  %d  valid public key : %d \n", publicKeyIndex, i);
       validPublicKeyArray[publicKeyIndex] = i;
       publicKeyIndex++;
+//<changed>
+      if(publicKeyIndex>maximumNumberOfPublicKeys){break;};
+//</changed>
     }
   }
   *sizeOfarrayArgument = sizeOfValidPublicKeyArray;
